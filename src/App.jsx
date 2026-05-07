610fc88bb8dfcf4dc97bc74a5da0b467ce236bab
giovannipastorini <giovannipastorini9@gmail.com>
2026-05-07 20:33:28 +0200

    Implement cat fetching functionality and display in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useEffect, useState } from 'react'
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  /* const [count, setCount] = useState(0) */
+  const [cats, setCats] = useState([])
+  const api_url="https://api.thecatapi.com/v1/breeds?limit=10&page=0"
+
+  useEffect(()=>{
+    console.log("component mounted");
+
+    fetch (api_url)
+     .then(response => response.json())
+     .then( data =>{
+        console.log(data);
+        setCats(data);
+    })
+
+    
+  }, [])
 
   return (
     <>
-      
+     <h1>Cat api</h1> 
+     <div>
+          {
+            cats.map( cat =>(
+              <div>
+                <ul>
+                  <li>{cat.name}</li>
+                </ul>
+              </div>
+            ))
+          }
+     </div>
     </>
   )
 }

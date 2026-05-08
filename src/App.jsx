29dc6c9a7d776aa7ecd10849fbf714fc8bc9286e
giovannipastorini <giovannipastorini9@gmail.com>
2026-05-08 05:01:43 +0200

    Add Bootstrap for styling and enhance App component layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react'
 function App() {
   /* const [count, setCount] = useState(0) */
   const [cats, setCats] = useState([])
+
   const api_url="https://api.thecatapi.com/v1/breeds?limit=10&page=0"
 
   useEffect(()=>{
@@ -14,6 +15,7 @@ function App() {
      .then(response => response.json())
      .then( data =>{
         console.log(data);
+
         setCats(data);
     })
 
@@ -21,20 +23,32 @@ function App() {
   }, [])
 
   return (
-    <>
-     <h1>Cat api</h1> 
-     <div>
-          {
-            cats.map( cat =>(
-              <div>
-                <ul>
-                  <li>{cat.name}</li>
-                </ul>
-              </div>
-            ))
-          }
-     </div>
-    </>
+    <main> 
+     <section>
+      <div className="container">
+        <div className="row g-4">
+          <h1>Cat API</h1> 
+            {
+              cats.map( cat =>(
+                <div className="col-sm-12 col-md-6 col-lg-3" key={cat.id}>
+                  <div className="card">
+                    <div className="card-body">
+                      <h2>{cat.name}</h2>
+                      <p>
+                        {cat.description}
+                      </p>
+                    </div>
+                      <div className="card-footer">
+                        <a href={cat.wikipedia_url}>find out more</a>
+                      </div>
+                  </div>
+                </div>
+              ))
+            }
+        </div>
+      </div>
+     </section>
+    </main>
   )
 }
 
